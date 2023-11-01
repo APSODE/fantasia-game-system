40680e9a7042e15622853ae3c0639888fdb97f97
APSODE <2gunbo74@gmail.com>
2023-11-02 05:19:40 +0900

    월급 지급 기능 추가 및 필요 없는 코드 제거

--- Fantasia.py
+++ Fantasia.py
@@ -1,28 +1,17 @@
 from typing import Optional, Dict
 
 from src.UserData import UserData
-from FantasiaSetting import USER_COLORS
+from FantasiaSetting import USER_COLORS, HERB_CARD, ANIMAL_CARD, TOOL_CARD
 
 
 class Fantasia:
     def __init__(self):
         self._user_datas: Dict[str, UserData] = {user_color: UserData.create_object(user_color) for user_color in USER_COLORS}
         self._round = 1
-        self._turn = 0  # 0 ~ 5는 각각의 유저에 대응
         self._tradeable_animals = {}
-
-    def get_current_turn_user(self) -> UserData:
-        return self._user_datas.get([key for key in self._user_datas.keys()][self._turn])
 
     def get_user(self, color: str) -> UserData:
         return self._user_datas.get(color)
-
-    def change_turn(self):
-        if self._turn == 5:
-            self._turn = 0
-
-        else:
-            self._turn += 1
 
     def round_change(self):
         self._round += 1
@@ -36,5 +25,22 @@ class Fantasia:
 
     def init_tradeable_animal(self):
         self._tradeable_animals.clear()
+
+    def apply_profit(self):
+        # 월급 지급 코드 (유저 전체)
+        for user_dto in self._user_datas.values():
+            total_profit = 0
+            for tool_dto in user_dto.tools.values():
+                if tool_dto.is_activated:
+                    total_profit += tool_dto.profit
+
+            for animal_dtos in user_dto.animals.values():
+                for animal_dto in animal_dtos:
+                    if animal_dto.amount > 0:
+                        total_profit += animal_dto.profit
+
+            total_profit -= user_dto.get_drugs_debuff()
+
+
 
 
